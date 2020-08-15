bf904f62630ed7a465b440b5152989db6339b6e9
Hugo <hsilva664>
2020-08-15 02:11:34 -0300

    a

--- scripts/amc_scripts2.sh
+++ scripts/amc_scripts2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#SBATCH -p mmaire-gpu --exclude=gpu-g6 -c 1 -J h_amp_search -d singleton --array=1-24
+#SBATCH -p mmaire-gpu --exclude=gpu-g6 -c 1 -J h_amc_search -d singleton --array=1-24
 
-bash -c ". ~/torchenv/bin/activate; sh ~/begin.sh; `sed "${SLURM_ARRAY_TASK_ID}q;d" scripts/amp_scripts2.txt`"
+bash -c ". ~/torchenv/bin/activate; sh ~/begin.sh; `sed "${SLURM_ARRAY_TASK_ID}q;d" scripts/amc_scripts2.txt`"
