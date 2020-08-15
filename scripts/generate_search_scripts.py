fb1bdba45486f9916fdfcc16846d0121c883fa75
Hugo <hsilva664>
2020-08-15 01:14:52 -0300

    fix

--- scripts/generate_search_scripts.py
+++ scripts/generate_search_scripts.py
@@ -8,5 +8,5 @@ for model in models:
     for seed in seeds:
         for wrem in wrem_list:
             in_dir = os.path.join('checkpoints', model, str(seed), 'training_state.tar' )
-            out_dir = os.path.join('checkpoints', model, str(seed), 'pruned.tar' )
+            out_dir = os.path.join('checkpoints', model, str(seed), 'pruned_{wrem}.tar'.format(wrem=wrem) )
             print("python3 amc_search.py --model={model} --dataset=cifar10 --data_root=./dataset/cifar10 --preserve_ratio={wrem} --lbound=0.2 --rbound=1 --reward=acc_reward --ckpt_path={in_dir} --seed={seed} --n_worker=2 --export_path={export_path}".format(model=model, wrem=wrem, in_dir=in_dir, seed=seed, export_path=out_dir))
